0e7779044c126831e053fbf76ca073067544baf1
ltdrobot <178366084+ltdrobot@users.noreply.github.com>
2025-03-10 08:12:37 +0000

    Deploying to gh-pages from @ ltdorgtest/cmake-docs-l10n@4d8d42cd10d862f16d8fe48b1767f51481e3fa13 🚀

---
+++ ko-kr/3.26/current.js
@@ -0,0 +1,5 @@
+var CURRENT_OPTIONS = {
+  CURRENT_VERSION   : '3.26',
+  CURRENT_LANGUAGE  : 'ko-kr',
+  HTML_BASEURL      : 'https://ltdorgtest.github.io/cmake-docs-l10n'
+};
---
+++ zh-cn/3.14/current.js
@@ -0,0 +1,5 @@
+var CURRENT_OPTIONS = {
+  CURRENT_VERSION   : '3.14',
+  CURRENT_LANGUAGE  : 'zh-cn',
+  HTML_BASEURL      : 'https://ltdorgtest.github.io/cmake-docs-l10n'
+};
